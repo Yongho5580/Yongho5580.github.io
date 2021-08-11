5863644cadcf683cd57424c806425d91ba2fc819
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-08-11 21:03:23 +0900

    add Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
 import * as React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 
 const Header = () => {
-  return <h1>header</h1>
+  return (
+    <Nav>
+      <NavLink to="/">Your Travel</NavLink>
+    </Nav>
+  )
 }
 
 export default Header
+
+const Nav = styled.div`
+  background: transparent;
+  height: 80px;
+`
+
+const NavLink = styled.div(Link)`
+
+`
